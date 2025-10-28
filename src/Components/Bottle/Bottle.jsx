5836acc0b79abb1e3c694e7754e69d2e0cff0a79
Sadia-d <subai>
2025-10-28 13:19:47 +0600

    style-with-Css

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Bottle.css'
 
-const Bottle = ({bottle}) => { 
+const Bottle = ({bottle,handleAddCart}) => { 
     const {name , img , price} = bottle;
+    console.log(bottle);
+    
     
     return (
         <div className='bottle'>
             <h3>Bottle : {name} </h3>
             <img src={img} alt="" srcset="" />
-            <h5>Price : {price}</h5>
+            <h5>Price : {price} $</h5>
+            <button onClick={handleAddCart}>Purches</button>
         </div>
     );
 };
--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -5,6 +5,7 @@ import './Bottles.css'
 const Bottles = () => {
 
     const [bottles , setBottles] = useState([]);
+    const[cart , setCart]=useState([])
 
     useEffect(() =>{
         fetch('bottle.json')
@@ -12,14 +13,25 @@ const Bottles = () => {
         .then(data => setBottles(data))
     } , [])
 
+    const handleAddCart = (bottle) =>{
+        const alreadyAded = cart.find((b) => b.name === bottle.name)
+        if(alreadyAded){
+            alert('already add this')
+            return
+        }
+        setCart(p =>[...p , bottle ])
+    }
 
     return (
         <div>
             <h3>Bottles here : {bottles.length}</h3>
+            <h4>Cart : {cart.length} </h4>
 
             <div className='bottles-container'>
                 {
-                    bottles.map(bottle => <Bottle bottle={bottle} ></Bottle>)
+                    bottles.map(bottle => <Bottle 
+                        handleAddCart={handleAddCart}
+                        bottle={bottle} ></Bottle>)
                 }
             </div>
         </div>
